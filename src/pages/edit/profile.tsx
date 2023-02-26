df74cf1ecb8e797dc318af4717a345307fd64b00
Jerrell Marvel <jerrell.marvel88@gmail.com>
2023-02-26 12:49:11 +0700

    handle edit profile error

--- src/pages/edit/profile.tsx
+++ src/pages/edit/profile.tsx
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { useQuery, useQueryClient } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
 type ProfileType = {
   user: {
@@ -17,8 +17,10 @@ const EditProfile: NextPage = () => {
   const router = useRouter();
   const queryClient = useQueryClient();
   const [profile, setProfile] = useState<ProfileType | undefined>(() => {
-    return queryClient.getQueryData(["edit", "profile"]);
+    return queryClient.getQueryData<ProfileType>(["edit", "profile"]);
   });
+
+  const [errMsg, setErrMsg] = useState("");
 
   const { data, isLoading, isError } = useQuery<ProfileType, any>({
     queryKey: ["edit", "profile"],
@@ -45,6 +47,32 @@ const EditProfile: NextPage = () => {
     retry: false,
   });
 
+  const { data: updateResponse, mutate: updateProfile } = useMutation<any, AxiosError>({
+    mutationFn: async () => {
+      const response = await axios.post<{ success: boolean }>(
+        "http://localhost:5000/api/v1/profile",
+        {
+          username: profile?.user.username,
+        },
+        { withCredentials: true }
+      );
+
+      return response.data;
+    },
+
+    onSuccess: () => {
+      router.push("/profile");
+    },
+
+    onError: (err) => {
+      // Do something with this error...
+      if (err.request.status === 400) {
+        setErrMsg("Name is already exist");
+      } else {
+        setErrMsg("Something went wrong please try again later");
+      }
+    },
+  });
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (profile) {
       setProfile({
@@ -73,21 +101,28 @@ const EditProfile: NextPage = () => {
   if (!isError) {
     return (
       <>
-        <input
-          type="text"
-          placeholder="name"
-          value={profile?.user.username}
-          onChange={(e) => {
-            onChangeHandler(e);
-          }}
-        />
-        <button
-          onClick={() => {
-            console.log(profile);
-          }}
-        >
-          save
-        </button>
+        <div>
+          <div>
+            <input
+              type="text"
+              placeholder="name"
+              value={profile?.user.username}
+              onChange={(e) => {
+                onChangeHandler(e);
+              }}
+            />
+            <button
+              onClick={() => {
+                updateProfile();
+                console.log(profile);
+              }}
+            >
+              save
+            </button>
+          </div>
+
+          <h1>{errMsg}</h1>
+        </div>
       </>
     );
   }
