a090d0c78ece62c3ae737b63d0d85f4bd2bddf5b
Jerrell Marvel <jerrell.marvel88@gmail.com>
2023-03-21 15:14:47 +0700

    add instagram and twitter field

--- src/pages/edit/profile.tsx
+++ src/pages/edit/profile.tsx
@@ -9,13 +9,18 @@ import { toast } from "react-toastify";
 type ProfileType = {
   user: {
     id: string;
-    username: string;
+    username: string | null;
     createdAt: string;
     updatedAt: string;
+    instagram: string | null;
+    twitter: string | null;
   };
 };
 
+type ProfileState = { username: string; instagram: string; twitter: string };
+
 const EditProfile: NextPage = () => {
+  const a: ProfileState = {} as ProfileState;
   const router = useRouter();
   const queryClient = useQueryClient();
   // const [profile, setProfile] = useState<ProfileType | undefined>(() => {
@@ -25,7 +30,11 @@ const EditProfile: NextPage = () => {
   const [getProfileErrMsg, setGetProfileErrMsg] = useState("");
   const [updateProfileErrMsg, setUpdateProfileErrMsg] = useState("");
 
-  const [controlledProfile, setControlledProfile] = useState<{ username: string }>({ username: "" });
+  const [controlledProfile, setControlledProfile] = useState<ProfileState>({
+    username: "",
+    instagram: "",
+    twitter: "",
+  });
 
   const {
     data: profile,
@@ -41,9 +50,14 @@ const EditProfile: NextPage = () => {
     onSuccess: (data) => {
       console.log(data);
       // setProfile(data);
-      if (data.user.username) {
-        setControlledProfile({ username: data.user.username });
-      }
+
+      const { username, instagram, twitter } = data.user;
+
+      setControlledProfile({
+        username: username || "",
+        instagram: instagram || "",
+        twitter: twitter || "",
+      });
 
       if (!data?.user.username) {
         // setHasUsername(false);
@@ -106,7 +120,8 @@ const EditProfile: NextPage = () => {
     }
 
     setControlledProfile({
-      username: e.target.value,
+      [e.target.name]: e.target.value,
+      ...controlledProfile,
     });
   };
 
@@ -130,23 +145,66 @@ const EditProfile: NextPage = () => {
               updateProfile();
             }}
           >
-            <div className="flex flex-col">
-              <label
-                htmlFor="name"
-                className="text-slate-700"
-              >
-                Change name
-              </label>
-              <input
-                type="text"
-                placeholder="name"
-                value={controlledProfile.username}
-                onChange={(e) => {
-                  onChangeHandler(e);
-                }}
-                id="name"
-                className="bg-slate-200 rounded-sm px-2 py-1 mt-2"
-              />
+            <div className="flex flex-col gap-2">
+              <div className="flex-col flex gap-1">
+                <label
+                  htmlFor="username"
+                  className="text-slate-700"
+                >
+                  Change name
+                </label>
+                <input
+                  type="text"
+                  placeholder="name"
+                  value={controlledProfile.username}
+                  onChange={(e) => {
+                    onChangeHandler(e);
+                  }}
+                  id="username"
+                  name="username"
+                  className="bg-slate-200 rounded-sm px-2 py-1"
+                />
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <label
+                  htmlFor="instagram"
+                  className="text-slate-700"
+                >
+                  Instagram
+                </label>
+                <input
+                  type="text"
+                  placeholder="instagram"
+                  value={controlledProfile.instagram}
+                  onChange={(e) => {
+                    onChangeHandler(e);
+                  }}
+                  id="instagram"
+                  name="instagram"
+                  className="bg-slate-200 rounded-sm px-2 py-1"
+                />
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <label
+                  htmlFor="username"
+                  className="text-slate-700"
+                >
+                  Twitter
+                </label>
+                <input
+                  type="text"
+                  placeholder="twitter"
+                  value={controlledProfile.twitter}
+                  onChange={(e) => {
+                    onChangeHandler(e);
+                  }}
+                  id="twitter"
+                  name="twitter"
+                  className="bg-slate-200 rounded-sm px-2 py-1"
+                />
+              </div>
             </div>
 
             <div className="flex justify-end">
