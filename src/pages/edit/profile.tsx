9f520052321a56dc19c7c6141e693fade6bee92a
Jerrell Marvel <jerrell.marvel88@gmail.com>
2023-03-21 16:12:14 +0700

    fix instagram and twitter field not send to the server

--- src/pages/edit/profile.tsx
+++ src/pages/edit/profile.tsx
@@ -86,6 +86,8 @@ const EditProfile: NextPage = () => {
         "http://localhost:5000/api/v1/profile",
         {
           username: controlledProfile.username,
+          instagram: controlledProfile.instagram,
+          twitter: controlledProfile.twitter,
         },
         { withCredentials: true }
       );
@@ -120,8 +122,9 @@ const EditProfile: NextPage = () => {
     }
 
     setControlledProfile({
-      [e.target.name]: e.target.value,
       ...controlledProfile,
+
+      [e.target.name]: e.target.value,
     });
   };
 
